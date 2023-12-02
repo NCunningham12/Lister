c08c594db617942f16e0332408b9c2a668d943ab
Nathan Cunningham <counterweight22@gmail.com>
2023-12-02 11:45:23 -0800

    Added Form and CSS styling

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const TodoForm = () => {
   return (
-    <div>TodoForm</div>
+    <form className='TodoForm'>
+      <input type='input' className='todo-input' placeholder='What is next on your list?' />
+      <button type='submit' className='todo-btn'>Add Task</button>
+    </form>
   )
 }
